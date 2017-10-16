5d6ded65a32f17c6b9cadcb5ffb139da443b563e
Aman <Amannijjar19@gmail.com>
2017-10-16 16:09:56 -0700

    Blinky LED
    Names

--- src/main.c
+++ src/main.c
@@ -14,6 +14,7 @@
 #include <stm32f0xx_rcc.h>
 #include "stm32f0xx_exti.h"
 #include "stm32f0xx_syscfg.h"
+#include "stm32f0xx.h"
 
 #include "FreeRTOSConfig.h"
 
@@ -27,8 +28,24 @@
 #pragma GCC diagnostic ignored "-Wmissing-declarations"
 #pragma GCC diagnostic ignored "-Wreturn-type"
 
+
+
 void blinkyTask(void *dummy){
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOC, ENABLE);
+	GPIOC->MODER |= 0x50000;
+	GPIOC->ODR |= 0x100; // 0001 0000 0000
+	vTaskDelay(2000);
+	GPIOC->ODR &= ~(0x100); //clears pin 8 (0b0000 0000 0000)
 	while(1){
+		GPIOC->ODR ^= 0x200;
+
+		/*0 1 2 3 4 5 6 7 8 9 A  B  C  D  E  F
+		0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
+		0b1111 -> 15 -> 0xF
+
+		0x3B5FA0 -> (# of bytes) = (number of digits) / 2*/
+
+		vTaskDelay(500);
 	}
 }
 
