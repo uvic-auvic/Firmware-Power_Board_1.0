cd341daf91ad6164bf4593781c348a68b0d65cb8
Aman <Amannijjar19@gmail.com>
2017-10-16 16:42:51 -0700

    BlinkLED

---
+++ inc/BlinkLED.h
@@ -0,0 +1,13 @@
+/*
+ * BlinkLED.h
+ *
+ *  Created on: Oct 16, 2017
+ *      Author: amann
+ */
+
+#ifndef BLINKLED_H_
+#define BLINKLED_H_
+
+void initBlinkyLED ();
+
+#endif /* BLINKLED_H_ */
--- src/main.c
+++ src/main.c
@@ -21,6 +21,8 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
+#include "BlinkLED.h"
+
 // Sample pragmas to cope with warnings. Please note the related line at
 // the end of this function, used to pop the compilfer diagnostics status.
 #pragma GCC diagnostic push
@@ -31,20 +33,11 @@
 
 
 void blinkyTask(void *dummy){
-	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOC, ENABLE);
-	GPIOC->MODER |= 0x50000;
-	GPIOC->ODR |= 0x100; // 0001 0000 0000
+	GPIOC->ODR |= 0x100;
 	vTaskDelay(2000);
-	GPIOC->ODR &= ~(0x100); //clears pin 8 (0b0000 0000 0000)
+	GPIOC->ODR &= ~(0x100);
 	while(1){
 		GPIOC->ODR ^= 0x200;
-
-		/*0 1 2 3 4 5 6 7 8 9 A  B  C  D  E  F
-		0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
-		0b1111 -> 15 -> 0xF
-
-		0x3B5FA0 -> (# of bytes) = (number of digits) / 2*/
-
 		vTaskDelay(500);
 	}
 }
@@ -61,10 +54,10 @@ void vGeneralTaskInit(void){
 int
 main(int argc, char* argv[])
 {
+	initBlinkyLED();
 	vGeneralTaskInit();
 	/* Start the kernel.  From here on, only tasks and interrupts will run. */
 	vTaskStartScheduler();
-
 	// Should never get here
 	while (1);
 }
