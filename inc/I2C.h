fd1e3fd606032da590b18c8388daae86c5ded66b
Poorna Chander Kalidas <poornakalidas@gmail.com>
2018-05-15 22:41:03 -0700

    Added I2C files and declared extern functions

---
+++ inc/I2C.h
@@ -0,0 +1,18 @@
+/*
+ * I2C.h
+ *
+ *  Created on: May 15, 2018
+ *      Author: Poornachander
+ */
+
+#ifndef I2C_H_
+#define I2C_H_
+
+extern void I2C_init();
+
+extern void I2C_read();
+
+extern void I2c_write();
+
+
+#endif /* I2C_H_ */
---
+++ src/I2C.c
@@ -0,0 +1,19 @@
+/*
+ * I2C.c
+ *
+ *  Created on: May 15, 2018
+ *      Author: Poornachander
+ */
+
+
+extern void I2C_init() {
+
+}
+
+extern void I2C_read() {
+
+}
+
+extern void I2C_write() {
+
+}
