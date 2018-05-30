d6437829e07d2b7fc5521b7581c593f18fd9ccc9
Poorna Chander Kalidas <poornakalidas@gmail.com>
2018-05-29 18:16:32 -0700

    Added header and source files for INA226 current sensor code

---
+++ src/INA226_Current_Sensor.c
@@ -0,0 +1,20 @@
+/*
+ * INA226_Current_Sensor.c
+ *
+ *  Created on: May 29, 2018
+ *      Author: Poornachander
+ */
+
+#include "stm32f0xx.h"
+
+void init_INA226_Current_Sensor() {
+
+}
+
+uint16_t update_system_current() {
+
+}
+
+uint16_t update_motor_current() {
+
+}
